5fc1570fb9285b127211e7b176a3e6b9182dfa10
Sathwic97 <sathwic97@gmail.com>
2023-07-30 00:01:42 +0530

    termgroup component completed

--- src/components/myflashcards_components/TermGroup.js
+++ src/components/myflashcards_components/TermGroup.js
@@ -1,24 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectAll } from '../Redux/FlashCardSlice';
+import { useParams } from 'react-router-dom';
+import { FaGreaterThan, FaLessThan } from 'react-icons/fa';
+import { AiOutlineArrowLeft, AiOutlineCopy, AiOutlineDownload, AiOutlinePrinter } from 'react-icons/ai';
+import { BsFacebook, BsWhatsapp } from 'react-icons/bs';
+import { AiFillLinkedin, AiOutlineTwitter, AiOutlineCheck, AiOutlineShareAlt } from 'react-icons/ai';
+import { CgMail } from 'react-icons/cg';
+import { TfiShare } from 'react-icons/tfi';
+import { IoMdClose } from 'react-icons/io';
+import { useNavigate } from 'react-router-dom';
+import {motion} from 'framer-motion'
 
 
 
 export const TermGroup = () => {
+    const { flashcardIndex } = useParams();
+    const navigate = useNavigate();
+    const flashcardId = parseInt(flashcardIndex);
+    const flashcards = useSelector((state) => selectAll(state));
+    const [currentSlide, setCurrentSlide] = useState(0);
+    const [showShareModal, setShowShareModal] = useState(false);
+    const [isCopied, setIsCopied] = useState(false);
+  
+    const totalPictures = flashcards[flashcardId]?.termGroup.length;
+  
+    const handleGoBack = () => {
+      navigate(-1)
+    }
+  
+    const handlePrevSlide = () => {
+      setCurrentSlide((prevSlide) => (prevSlide + totalPictures - 1) % totalPictures);
+    };
+  
+    const handleNextSlide = () => {
+      setCurrentSlide((prevSlide) => (prevSlide + 1) % totalPictures);
+    };
+  
+    const handleTermClick = (index) => {
+      setCurrentSlide(index);
+    };
+  
+    const handleShare = () => {
+      setShowShareModal(true);
+    };
+  
+    const handleCloseShareModal = () => {
+      setShowShareModal(false);
+    };
+  
+    const handleCopy = () => {
+      setIsCopied(true);
+      setTimeout(() => {
+        setIsCopied(false);
+      }, 2000);
+    };
+  
+    const handleDownload = () => {
+      const link = document.createElement('a');
+      link.href = flashcards[flashcardId]?.termGroup[currentSlide]?.termImage?.termImageURL;
+      link.download = 'flashcard.png';
+      link.click();
+    };
+  
+    const handlePrint = () => {
+      window.print();
+    };
+  
   
   return (
     <div   className='p-5'>
       <div>
         <div className='flex flex-row'>
           <div className='mr-3'>
-            <button className='text-[15px]'  /*here on click for going back */ >
-              {/* left arrow icon here */}
+            <button className='text-[15px]'  onClick={handleGoBack} >
+            <AiOutlineArrowLeft />
             </button>
           </div>
           <div className='mb-4'>
             <h1 className='text-[18px] font-bold mb-3'>
-              {/* here the mainGroupName */}
+            {flashcards[flashcardId]?.mainGroup?.mainGroupName}
             </h1>
             <p className='text-[13px] '>
-              {/* here the mainGroupDescription */}
+            {flashcards[flashcardId]?.mainGroup?.mainGroupDescription}
             </p>
           </div>
         </div>
@@ -28,91 +92,100 @@ export const TermGroup = () => {
             <div className='border-b-2 flex justify-center'>
               <h1 className='text-[16px] mt-3 mb-3'>Flashcards</h1>
             </div>
-            {/* here we have to display the term names in the specific flashcards of the mainGroup */}
-              <div className='text-2xl flex ml-5' >
+            {flashcards[flashcardId]?.termGroup.map((term, index) => (
+              <div className='text-2xl flex ml-5' key={index}>
                 <h2
-                    /*make the styleing for the term name , the active term name must be in the red color*/
+                  className={`${currentSlide === index ? 'text-red-500' : ''}`}
+                  onClick={() => handleTermClick(index)}
+                  style={{ marginTop: '3px', marginLeft: '5px', fontSize: '13px', cursor: 'pointer' }}
                 >
-                  {/* here the termName comes  */}
+                  {term.termGroupName}
                 </h2>
               </div>
+            ))}
+            
 
           </div>
 
           <div className='bg-white flex flex-row w-4/5 h-80 p-20px'>
-            <img
-            /* here add the image and make a image carousel */
-              src=''
+            <motion.img
+              src={flashcards[flashcardId]?.termGroup[currentSlide]?.termImage?.termImageURL}
               className='ml-4 w-1/2 h-64 mt-5'
               alt='term img'
 
             />
             <p className='ml-4 text-[13px] w-1/2 h-72 mt-5'>
-              {/* here has to be termGroupDescription */}
+            {flashcards[flashcardId]?.termGroup[currentSlide]?.termGroupDescription}
             </p>
           </div>
 
-            {/* Make on Click handles to every button and sutable icons before the text , to every button under this comment */}
+          
           <div className='flex flex-col items-left space-y-3'>
-            <button className='w-36 p-2 text-[15px] text-black bg-white flex items-center space-x-2' >
-              
+            <button className='w-36 p-2 text-[15px] text-black bg-white flex items-center space-x-2' onClick={handleShare} >
+            <TfiShare />
               <span className='ml-4'>Share</span>
             </button>
-            <button className='w-36 p-2 text-[15px] text-black bg-white flex items-center space-x-2' >
-
-              <span className='ml-4'>Download</span>
+            <button className='w-36 p-2 text-[15px] text-black bg-white flex items-center space-x-2' onClick={handleDownload} >
+            <AiOutlineDownload />
+            <span className='ml-4'>Download</span>
             </button>
-            <button className='w-36 p-2 text-[15px] text-black bg-white flex items-center space-x-2' >
-
-              <span className='ml-4'>Print</span>
+            <button className='w-36 p-2 text-[15px] text-black bg-white flex items-center space-x-2' onClick={handlePrint} >
+            <AiOutlinePrinter />
+            <span className='ml-4'>Print</span>
             </button>
           </div>
         </div>
-        {/* Make on Click handles to every button and sutable icons before the text , to every button under this comment */}
-        <div className='flex flex-row justify-center mt-3 space-x-3'>
+        <div className='flex flex-row justify-center mt-3 space-x-3' onClick={handlePrevSlide}>
           <button className='text-[15px]' >
-            {/* here  make the lessthan icon */}
+          <FaLessThan />
           </button>
           <p className='text-[13px]'>
-            {/* here it has to show the no of slices and indexes */}
+          {currentSlide + 1} / {flashcards[flashcardId]?.termGroup?.length || 0}
           </p>
-          <button className='text-[15px]' >
-            {/* here make the greater than icon */}
+          <button className='text-[15px]' onClick={handleNextSlide} >
+          <FaGreaterThan />
           </button>
         </div>
       </div>
 
-      {/* this is the modal it has to get visible when click of the share button */}
+      {showShareModal && (
         <div className='fixed top-0 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center'>
           <div className='bg-white w-80 h-48 p-2'>
-            <button  className='text-[12px] hover:text-red-500 float-right'>
-              {/* here the closed icon */}
+            <button  className='text-[12px] hover:text-red-500 float-right' onClick={handleCloseShareModal} >
+            <IoMdClose />
             </button>
             <h2 className='text-[15px] font-bold mt-4 ml-6'>Share</h2>
             <div className='flex flex-row'>
               <input type='text' className='w-auto p-2 text-[10px] ml-2 mt-6 mb-6 border border-grey-300' value=' https://example.com/flashcards  ' readOnly />
-              <button  className='text-[13px]'>
-                {/* make a copy button here */}
+              <button onClick={handleCopy} className='text-[13px]'>
+              {isCopied ? <AiOutlineCheck /> : <AiOutlineCopy />}
               </button>
               <button className='text-[13px]'>
-                {/* share icon here  */}
+              <AiOutlineShareAlt />
               </button>
             </div>
             <div className='flex flex-row space-x-5 justify-center mt-4'>
-              {/* here make a facebook icon, it have to open the facebook page when they click the icon */}
-              
-              {/* here make a facebook icon, it have to open the whatsapp page when they click the icon */}
-             
-              {/* here make a facebook icon, it have to open the linkDin page when they click the icon */}
-
-              {/* here make a facebook icon, it have to open the gmail page when they click the icon */}
-
-              {/* here make a facebook icon, it have to open the whatsapp page when they click the icon */}
+            <a href='https://www.facebook.com' target='_blank' rel='noreferrer'>
+                <BsFacebook className='text-[25px] text-blue-500' />
+              </a>
+              <a href='https://www.whatsapp.com' target='_blank' rel='noreferrer'>
+                <BsWhatsapp className='text-[25px] text-green-500' />
+              </a>
+              <a href='https://www.linkedin.com' target='_blank' rel='noreferrer'>
+                <AiFillLinkedin className='text-[25px] text-blue-600' />
+              </a>
+              <a href='https://www.gmail.com' target='_blank' rel='noreferrer'>
+                <CgMail className='text-[25px] text-red-500 ' />
+              </a>
+              <a href='https://www.twitter.com' target='_blank' rel='noreferrer'>
+                <AiOutlineTwitter className='text-[25px] text-blue-300' />
+              </a>
 
             </div>
             <div className='mt-3'></div>
           </div>
         </div>
+        )}
 
     </div>
   );
