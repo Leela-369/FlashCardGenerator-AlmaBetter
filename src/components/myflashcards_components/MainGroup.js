f17d2f7f132220224d66e7a94f58124faca24985
Leela-369 <gonnabathulaleelasanthosh@gmail.com>
2023-07-30 23:47:19 +0530

    added localStoreage and Comments

--- src/components/myflashcards_components/MainGroup.js
+++ src/components/myflashcards_components/MainGroup.js
@@ -7,32 +7,42 @@ import { Link } from 'react-router-dom'
 
 
 export const MainGroup = () => {
+    // Get  flashcard state from the Redux store
 const flashcards = useSelector((state) => selectAll(state));
 
+  // State to toggle between showing all cards and a limited number of cards
 const [showAllCards, setShowAllCards] = useState(false);
-
+ 
+// Maximum number of cards to show when not in "Show All" mode
 const maxVisibleCards = 6;
 
+  // Function to toggle between showing all cards and a limited number of cards
 const handleSeeAll = () => {
   setShowAllCards(!showAllCards);
 };
 
+  // Determine which flashcards to display based on "showAllCards" state
 const displayedFlashcards = showAllCards ? flashcards : flashcards.slice(0, maxVisibleCards);
     
   return (
     <div>
       <div
        className='flex flex-wrap gap-3 ml-2'
-       
        >
+          {/* Display each flashcard */}
         {displayedFlashcards.map((flashcard, flashcardIndex) => (
             <div key={flashcardIndex}  className='flex flex-shrink-0'>
               <div className='flex flex-col items-center'>
+                {/* Display the MainGroup Image */}
               <img src={flashcard?.mainGroup?.mainGroupImage?.mainImageURL} alt='' className=' w-12 h-12 rounded-full z-10 position relative top-3 shadow-md'/>
               <div className='bg-white w-72 h-48 flex flex-col space-y-3 items-center z-0'>
+                {/* Display the mainGroup Name */}
                 <h1 className='text-[15px] font-bold mt-8'>{flashcard?.mainGroup?.mainGroupName}</h1>
+                {/* Display the mainGroup Description */}
                 <p className='text-[10px] text-center'>{flashcard?.mainGroup?.mainGroupDescription} </p>
+                {/* Display the lenth of the terms the user made in the current flashcard */}
                 <p className='text-[13px]'>{flashcard.termGroup.length} cards</p>
+                {/* This the Link button to the TermGroup */}
                 <nav>
                 <Link to={`/termGroup/${flashcardIndex}`}>
               <button className='w-36 h-8 text-red-500 text-[13px] p-1 border border-red-500'>View Cards</button>
@@ -43,6 +53,7 @@ const displayedFlashcards = showAllCards ? flashcards : flashcards.slice(0, maxV
               </div>
           ))}
           </div>
+          {/* See All Button and The Logic when the see all button has to be appeared */}
               {!showAllCards && flashcards.length > maxVisibleCards && (
         <button 
          className=' text-red-500 mt-4 float-right '
--- src/features/flashCardSlice.js
+++ src/features/flashCardSlice.js
@@ -1,9 +1,20 @@
 // Import the 'createSlice' function from the '@reduxjs/toolkit' package.
 import { createSlice } from "@reduxjs/toolkit";
 
+// Retrieve data from local storage and parse it as JSON.
+const localStoreData = localStorage.getItem("addFlashcard");
+let parsedData = null;
+
+try {
+  parsedData = JSON.parse(localStoreData);
+} catch (error) {
+  console.error("Error parsing JSON:", error);
+}
+
+
 // Define the initial state of the flashcard slice.
 const initialState = {
-  flashcards: [],
+  flashcards: parsedData ? parsedData : [], // Set the initial state to parsedData if available, otherwise an empty array.
 };
 
 // Created 'flashcardSlice' slice.
@@ -17,6 +28,8 @@ const flashcardSlice = createSlice({
       // It receives the 'state' and the 'action' payload as arguments.
       // The new flashcard object is pushed to the 'flashcards' array in the state.
       state.flashcards.push(action.payload);
+      //set the addFlashcard to the local storeage 
+      localStorage.setItem("addFlashcard", JSON.stringify(state.flashcards));
     },
   },
 });
